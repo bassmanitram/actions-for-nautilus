10d06ff2d2358b62702c73a066a1c10af2d90e2f
Martin Bartlett <Martin Bartlett@dxc.com>
2022-07-13 13:37:35 +0200

    Comment out debug logging

--- configurator/start-configurator.sh
+++ configurator/start-configurator.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 _MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-env | sort >> /tmp/a4n-start.log
+#env | sort >> /tmp/a4n-start.log
 
 cd $_MY_DIR
-echo $PWD >> /tmp/a4n-start.log
+#echo $PWD >> /tmp/a4n-start.log
 
 #
 # Start the configuration server, the xdg-open the home page
@@ -13,27 +13,27 @@ echo $PWD >> /tmp/a4n-start.log
 #
 # Kill existing instance
 #
-pgrep -U $USER -f "python ./actions-for-nautilus-configurator.py" >> /tmp/a4n-start.log 2>&1
+#pgrep -U $USER -f "python ./actions-for-nautilus-configurator.py" >> /tmp/a4n-start.log 2>&1
 pkill -U $USER -f "python ./actions-for-nautilus-configurator.py"
 RC=$?
-echo "after the kill - $RC" >> /tmp/a4n-start.log
+echo "after the kill - $RC" # >> /tmp/a4n-start.log
 
 #
 # Find a port
 #
 PORT=$(python ./find-a-port.py)
-echo "the port $PORT" >> /tmp/a4n-start.log 
+echo "the port $PORT" # >> /tmp/a4n-start.log 
 
 #
 # Start the server and detatch
 #
-python ./actions-for-nautilus-configurator.py $PORT >> /tmp/a4n-start.log 2>&1 &
+python ./actions-for-nautilus-configurator.py $PORT & # >> /tmp/a4n-start.log 2>&1 &
 RC=$?
-echo "server started $RC" >> /tmp/a4n-start.log
+echo "server started $RC" # >> /tmp/a4n-start.log
 
 #
 # Start web page
 #
-xdg-open http://localhost:$PORT >> /tmp/a4n-start.log 2>&1
+xdg-open http://localhost:$PORT # >> /tmp/a4n-start.log 2>&1
 RC=$?
-echo "XDG done $RC" >> /tmp/a4n-start.log
+echo "XDG done $RC" # >> /tmp/a4n-start.log
