02c2a6efa992f834446a413c8106205cb74faf08
Martin Bartlett <Martin Bartlett@dxc.com>
2022-07-23 14:42:50 +0200

    remove echos

--- configurator/start-configurator.sh
+++ configurator/start-configurator.sh
@@ -17,24 +17,24 @@ mkdir -p $HOME/.local/share/actions-for-nautilus
 #pgrep -U $USER -f "python ./actions-for-nautilus-configurator.py" >> /tmp/a4n-start.log 2>&1
 pkill -U $USER -f "python ./actions-for-nautilus-configurator.py"
 RC=$?
-echo "after the kill - $RC" # >> /tmp/a4n-start.log
+#echo "after the kill - $RC" # >> /tmp/a4n-start.log
 
 #
 # Find a port
 #
 PORT=$(python ./find-a-port.py)
-echo "the port $PORT" # >> /tmp/a4n-start.log 
+#echo "the port $PORT" # >> /tmp/a4n-start.log 
 
 #
 # Start the server and detatch
 #
 python ./actions-for-nautilus-configurator.py $PORT & # >> /tmp/a4n-start.log 2>&1 &
 RC=$?
-echo "server started $RC" # >> /tmp/a4n-start.log
+#echo "server started $RC" # >> /tmp/a4n-start.log
 
 #
 # Start web page
 #
 xdg-open http://localhost:$PORT # >> /tmp/a4n-start.log 2>&1
 RC=$?
-echo "XDG done $RC" # >> /tmp/a4n-start.log
+#echo "XDG done $RC" # >> /tmp/a4n-start.log
