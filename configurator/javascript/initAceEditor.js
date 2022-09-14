6181231f9253e3e6f7e28f07086b5722c6458112
Martin Bartlett <Martin Bartlett@dxc.com>
2022-09-14 14:36:25 +0200

    Bit of refactoring to help with the max-height

--- configurator/javascript/initAceEditor.js
+++ configurator/javascript/initAceEditor.js
@@ -5,7 +5,55 @@ let draggingAceEditor = false;
 let draggingEditorTopOffset = 0;
 let aceEditorResizeBar;
 let aceEditorWrapper;
+let aceEditorWrapperExtra;
 let aceEditor;
+
+/*
+ * And the way we handle JSON editors
+ */
+JSONEditor.defaults.editors.object.prototype.showEditJSON = function() {
+	if (!this.editjson_holder) return
+	this.hideAddProperty()
+
+
+	/* Start the textarea with the current value */
+	this.ace_editor.setValue(JSON.stringify(convertToBackendFormat(this.getValue()), null, 4));
+
+	/* Disable the rest of the form while editing JSON */
+	this.disable()
+
+	this.editjson_holder.style.display = ''
+	this.editjson_control.disabled = false
+	this.editing_json = true
+
+	aceEditorWrapperExtra = aceEditorWrapper.offsetHeight - aceEditor.offsetHeight;
+	console.log(aceEditorWrapperExtra,aceEditorWrapper.offsetHeight,aceEditor.offsetHeight);
+} 
+JSONEditor.defaults.editors.object.prototype.copyJSON = function() {
+	if (!this.editjson_holder) return
+	const ta = document.createElement('textarea')
+	ta.value = JSON.stringify(convertToFrontendFormat(JSON.parse(this.ace_editor.getValue())))
+	ta.setAttribute('readonly', '')
+	ta.style.position = 'absolute'
+	ta.style.left = '-9999px'
+	document.body.appendChild(ta)
+	ta.select()
+	document.execCommand('copy')
+	document.body.removeChild(ta)
+}
+JSONEditor.defaults.editors.object.prototype.saveJSON = function() {
+	if (!this.editjson_holder) return	
+	try {
+	  const json = JSON.parse(this.ace_editor.getValue())
+	  this.setValue(convertToFrontendFormat(json))
+	  this.hideEditJSON()
+	  this.onChange(true)
+	  } catch (e) {
+		console.log(e)
+	  window.alert('invalid JSON')
+	  throw e
+	}
+}
 
 function aceDragMouseMove (e) {
 	// editor height
@@ -23,6 +71,8 @@ function aceDragMouseDown(e) {
 	draggingAceEditor = true;
 
 	draggingEditorTopOffset = e.pageY - aceEditor.offsetHeight;
+
+	//let absTop = e.screenY - aceEditor.offsetHeight
 
 	// Set editor opacity to 0 to make transparent so our aceEditorWrapper div shows
 	aceEditor.style.opacity = 1;
