c93f3eea2cc4295f3536ad7d6b38a6ca056e9db8
Martin Bartlett <jbartle9@dxc.com>
2025-05-19 17:55:29 +0200

    Doc and image updates (incomplete)

--- configurator/javascript/constants.js
+++ configurator/javascript/constants.js
@@ -4,6 +4,7 @@ const iconNames = {
 }
 
 const infoText = {
+	"#action:toolbar": { "text": "Manage the menu", "help_label": "action-toolbar" },
 	"#action": { "text": "An entry that will appear in the Nautilus context menu. Such an entry can be a Command or a nested Menu. Click for more info.", "help_label": "menu-actions" },
 	"#command:command_line": { "text": "The command line to execute when the action is clicked on.  Click for more info.", "help_label": "commands" },
 	"#command:cwd": { "text": "The current working directory to set when executing the command. Placeholders are allowed. Click for more info.", "help_label": "cwd" },
@@ -12,6 +13,7 @@ const infoText = {
 	"#command:filetypes": { "text": "A list of file types for which the action will be displayed, or not be displayed in the event of a '!' prefix. Click for more info.", "help_label": "file-types" },
 	"#command:label": { "text": "The label that will appear in the context menu for this command. Click for more info.", "help_label": "command-label" },
 	"#command:interpolation": { "text": "How to interpolate placeholders into the command line. Click for more info.", "help_label": "placeholder-interpolation" },
+	"#command:min_items": { "text": "The minimum number of items in the selection for which this action will be displayed. The default is 1. Must be less than or equal to max_items if max_items is greater than zero. Click for more info.", "help_label": "min-items" },
 	"#command:min_items": { "text": "The minimum number of items in the selection for which this action will be displayed. The default is 1. Must be less than or equal to max_items if max_items is greater than zero. Click for more info.", "help_label": "min-items" },
 	"#command:max_items": { "text": "The maximum number of items in the selection for which this action will be displayed. Zero indicates unlimited. The default is zero. If not zero, must be greater than or equal to min_items. Click for more info.", "help_label": "max-items" },
 	"#command:mimetypes": { "text": "A list of standard mimetype specifications for which the action will be displayed, or not be displayed in the event of a '!' prefix. Click for more info.", "help_label": "mimetypes" },
@@ -27,6 +29,7 @@ const infoText = {
 	"#submenu:disabled": { "text": "If the menu is marked as disabled it and its items will not appear in the Gnome Files context menu", "help_label": "action-disable" },
 	"#submenu:label": { "text": "The label that will appear in the context menu for this sub menu. Click for more info.", "help_label": "submenu-label" },
 	"#submenu:sort": { "text": "Whether actions should be sorted automatically or left in the configured order. Click for more info.", "help_label": "submenu-sort" },
+	"#submenu:toolbar": { "text": "Manage the menu", "help_label": "menu-toolbar" },
 	"#submenu": { "text": "An entry in the Nautilus context menu or sub menu that, when clicked on, results in a sub menu being displayed. Click for more info.", "help_label": "menu-actions" },
 	"#command:permissions": { "text": "The minimum access permissions that you must have for the selected files in order for the command to be displayed. Click for more info.", "help_label": "permissions" }
 }
